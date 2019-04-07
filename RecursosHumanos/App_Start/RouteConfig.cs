a2fb7722a170370164fe7d14457cdb937c5650ef
DanielRincon99 <49107349+DanielRincon99@users.noreply.github.com>
2019-04-07 14:29:00 -0400

    Controlador de Departamento

--- RecursosHumanos/App_Start/RouteConfig.cs
+++ RecursosHumanos/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace RecursosHumanos
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Departamentos", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
